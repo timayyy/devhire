28f8039923e540d3608b44a76db8a671313dbc6b
timayyy <timilehin65@gmail.com>
2022-04-19 06:53:04 +0100

    fixed select input value change

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -2,28 +2,28 @@ import { StyledCustomSelect } from './select.style'
 import { MdKeyboardArrowDown } from 'react-icons/md'
 import { useDispatch, useSelector } from 'react-redux'
 import { updateCurrency } from '../../redux/currency/currency.actions'
-import { useState } from 'react'
 import { Spinner } from '../spinner/spinner'
 
 const SelectInput = () => {
-    const [currencyOption, setCurrencyOption] = useState('')
 
     const dispatch = useDispatch()
 
     const currencyList = useSelector((state) => state.currencyList)
     const { loading, currencies } = currencyList
 
+    const currentCurrency = useSelector((state) => state.currentCurrency)
+    const { currency } = currentCurrency
+
 
     const handleSelectChange = (e) => {
         const id = e.target.value
-        setCurrencyOption(id)
         dispatch(updateCurrency(Number(id)))
     }
     return (
         <StyledCustomSelect>
             {loading ? <Spinner /> : (
                 <>
-                    <select value={currencyOption} onChange={handleSelectChange}>
+                    <select value={currency.id} onChange={handleSelectChange}>
                         {currencies.map((currency, index) => (
                             <option key={index} value={currency.id}>{currency.name}</option>
                         ))}
